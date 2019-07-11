4e394dfcd4b175ba0333ad06704c9ecbd0e09ba7
shensheng070632 <1606914250@qq.com>
2019-07-11 11:49:10 +0800

    zhajinHua files are commited

---
+++ zhaJinHua/ai.c
@@ -0,0 +1,8 @@
+#include"../all.h"
+int main(){
+    int n = 0;
+    while(1){
+        sleep(1);
+        printf("I am a robot, %d\n",n++);
+    }
+}
---
+++ zhaJinHua/protocol.h
@@ -0,0 +1,8 @@
+#ifndef __COMM_H__
+#define __COMM_H__
+
+#define replyTrash -1
+#define replyOnlineConfirm 0x06
+
+
+#endif
